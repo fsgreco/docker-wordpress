93ca0a7e41a5c3f6001cf8d3171b965e7f45d156
Santiago <fsgreco@fastmail.com>
2023-06-10 13:44:18 +0200

    ADD potential fix in case DB has custom table_prefix

--- dump/update_db_siteurl_home.sh
+++ dump/update_db_siteurl_home.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Beware - if you have a custom WP TABLE_PREFIX remember to change `wp_options` for `wp_prefix_options`
 mysql -u wp_user -pwp_pass -D wp_wordpress <<EOF
 UPDATE wp_options 
 SET option_value = "http://localhost:8000"
