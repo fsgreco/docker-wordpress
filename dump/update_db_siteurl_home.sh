b03992805c8f2314df38749678601feb055df983
Santiago <fsgreco@fastmail.com>
2023-11-14 23:49:17 +0100

    Improve update script on /dump

--- dump/update_db_siteurl_home.sh
+++ dump/update_db_siteurl_home.sh
@@ -8,4 +8,12 @@ WHERE option_name = "siteurl";
 UPDATE wp_options 
 SET option_value = "http://localhost:8000"
 WHERE option_name = "home";
+
+UPDATE wp_options
+SET option_value = "LOCALHOST DEV"
+WHERE option_name = "blogname";
 EOF
+
+# UPDATE wp_options
+# SET option_value = "YOUR_AT_EMAIL_TLD"
+# WHERE option_name = "admin_email";
